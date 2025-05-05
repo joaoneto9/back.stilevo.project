6cfa1c1fbdf5f3bb91c60ff51a599dee2b4425ef
joaoneto9 <joaonetocg1@gmail.com>
2025-05-05 17:24:41 -0300

    feat: implemnetando uma classe de teste para o banco de dados. testando e verificando que as requisicoes GET estao funcionando.                                                                   fix: nao pode colocar a notacao @mapper, em uma interface generic. ajustado

--- src/main/java/com/stilevo/store/back/stilevo/project/api/mapper/ProductVariationMapper.java
+++ src/main/java/com/stilevo/store/back/stilevo/project/api/mapper/ProductVariationMapper.java
@@ -3,6 +3,8 @@ package com.stilevo.store.back.stilevo.project.api.mapper;
 import com.stilevo.store.back.stilevo.project.api.domain.dto.request.ProductRequestDTO;
 import com.stilevo.store.back.stilevo.project.api.domain.dto.response.ProductResponseDTO;
 import com.stilevo.store.back.stilevo.project.api.domain.entity.Product;
+import org.mapstruct.Mapper;
 
+@Mapper(componentModel = "spring")
 public interface ProductVariationMapper extends GeneralMapper<Product, ProductRequestDTO, ProductResponseDTO> {
 }
