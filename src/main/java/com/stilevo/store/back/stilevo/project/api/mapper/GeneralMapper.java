74a5f3782009b9c1b30e4b404897e0dafc56d5ab
joaoneto09 <joaovneto@aluno.colegiomotiva.com.br>
2025-05-26 18:56:02 -0300

    fix: resolvendo o Bug no mapeamento da lista de OrderItems de uma Order, estava retornando nulo, ao inves da lista com os Objetos

--- src/main/java/com/stilevo/store/back/stilevo/project/api/mapper/GeneralMapper.java
+++
@@ -1,6 +0,0 @@
-package com.stilevo.store.back.stilevo.project.api.mapper;
-
-public interface GeneralMapper<Entity, EntityRequestDTO, EntityResponseDTO> {
-    Entity toEntity(EntityRequestDTO entityRequestDTO);
-    EntityResponseDTO toResponse(Entity entity);
-}
