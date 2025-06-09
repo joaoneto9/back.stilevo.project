020e561ee05afa3bb031f819d1ac49452ac75945
joaoneto9 <joaonetocg1@gmail.com>
2025-06-09 17:27:16 -0300

    feat: implementando umaQuery personalizada para consultar um ProductVAriation e seu produto especificado de forma EAGER, evitando N + 1 queries no banco e denotando um ftch Lazy entre Cart e User.

--- src/main/java/com/stilevo/store/back/stilevo/project/api/domain/repository/CartRepository.java
+++ src/main/java/com/stilevo/store/back/stilevo/project/api/domain/repository/CartRepository.java
@@ -1,6 +1,7 @@
 package com.stilevo.store.back.stilevo.project.api.domain.repository;
 import com.stilevo.store.back.stilevo.project.api.domain.entity.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface CartRepository extends JpaRepository<Cart, Long> {
 }
--- src/main/java/com/stilevo/store/back/stilevo/project/api/domain/repository/ProductVariationRepository.java
+++ src/main/java/com/stilevo/store/back/stilevo/project/api/domain/repository/ProductVariationRepository.java
@@ -2,6 +2,13 @@ package com.stilevo.store.back.stilevo.project.api.domain.repository;
 
 import com.stilevo.store.back.stilevo.project.api.domain.entity.ProductVariation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ProductVariationRepository extends JpaRepository<ProductVariation, Long> {
+
+    // busca o produto variado acompanhado do produto ja.
+    @Query("SELECT variation FROM ProductVariation variation JOIN FETCH variation.product")
+    List<ProductVariation> findAllVariationsOfProducts();
 }
