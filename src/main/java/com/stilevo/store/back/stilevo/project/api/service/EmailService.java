0c24787750979ca7760b9aac7094bc237f76cd7d
joaoneto9 <joaonetocg1@gmail.com>
2025-06-22 12:26:24 -0300

    feat: adicionando a dependencia do Email Sender  e criando o Email Sevrice paar envio de Emails

---
+++ src/main/java/com/stilevo/store/back/stilevo/project/api/service/EmailService.java
@@ -0,0 +1,7 @@
+package com.stilevo.store.back.stilevo.project.api.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+}
--- src/test/java/com/stilevo/store/back/stilevo/project/test/services/UserServiceTest.java
+++ src/test/java/com/stilevo/store/back/stilevo/project/test/services/UserServiceTest.java
@@ -211,7 +211,4 @@ public class UserServiceTest {
         }
     }
 
-
-
-
 }
