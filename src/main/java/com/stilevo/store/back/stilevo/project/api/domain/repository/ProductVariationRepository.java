a933c81043222ad889ce5593ce415991ec6bc9ea
joaoneto09 <joaovneto@aluno.colegiomotiva.com.br>
2025-07-23 00:17:36 -0300

    feat: implementing the a cash politic at the api of products variations

--- src/main/java/com/stilevo/store/back/stilevo/project/api/domain/repository/ProductVariationRepository.java
+++ src/main/java/com/stilevo/store/back/stilevo/project/api/domain/repository/ProductVariationRepository.java
@@ -1,27 +1,14 @@
 package com.stilevo.store.back.stilevo.project.api.domain.repository;
 
 import com.stilevo.store.back.stilevo.project.api.domain.entity.ProductVariation;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-
-import java.util.List;
 
 public interface ProductVariationRepository extends JpaRepository<ProductVariation, Long> {
 
     // busca o produto variado acompanhado do produto ja, por meio do JOIN FETCH, evita N + 1 consultas.
     @Query("SELECT variation FROM ProductVariation variation JOIN FETCH variation.product")
-    List<ProductVariation> findAllVariationsWithProducts();
-
-    /*
-    *CONCAT('%', :nome, '%') serve para montar uma busca "parecida" (semelhante)
-    *, verificando se a palavra aparece em qualquer lugar do nome.
-    *
-    * Se quiser:
-    *
-    * Começando com o nome: CONCAT(:nome, '%')
-    * Terminando com o nome: CONCAT('%', :nome)
-    */
-    @Query("SELECT variation FROM ProductVariation variation " +
-            "JOIN FETCH variation.product WHERE LOWER(variation.product.name) LIKE LOWER(CONCAT('%', :name, '%'))")
-    List<ProductVariation> findAllBySimilarName(String name);
+    Page<ProductVariation> findAllVariationsWithProducts(Pageable pageable);
 }
--- src/main/java/com/stilevo/store/back/stilevo/project/api/service/ProductVariationService.java
+++ src/main/java/com/stilevo/store/back/stilevo/project/api/service/ProductVariationService.java
@@ -7,15 +7,20 @@ import com.stilevo.store.back.stilevo.project.api.domain.entity.Product;
 import com.stilevo.store.back.stilevo.project.api.domain.entity.ProductVariation;
 import com.stilevo.store.back.stilevo.project.api.domain.repository.ProductVariationRepository;
 import com.stilevo.store.back.stilevo.project.api.mapper.ProductVariationMapper;
+import com.stilevo.store.back.stilevo.project.api.service.cache.contract.CacheKeyPairContract;
+import com.stilevo.store.back.stilevo.project.api.service.cache.structure.CacheKeyPairImpl;
 import jakarta.persistence.EntityNotFoundException;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotNull;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.stereotype.Service;
-import java.util.List;
+
 
 @Service
 public class ProductVariationService {
+
+    // tentativa de cache
+    private CacheKeyPairContract<Long, ProductVariation> cache;
 
     private final ProductVariationMapper productVariationMapper;
     private final ProductVariationRepository productVariationRepository;
@@ -25,27 +30,26 @@ public class ProductVariationService {
         this.productVariationMapper = productVariationMapper;
         this.productVariationRepository = productVariationRepository;
         this.productService = productService;
+        this.cache = new CacheKeyPairImpl<>();
     }
 
     @Transactional(readOnly = true)
-    public List<ProductVariationResponseDTO> findAll() {
-        return productVariationRepository.findAll().stream()
-                .map(productVariationMapper::toResponse)
-                .toList();
-    }
-
-    @Transactional(readOnly = true)
-    public List<ProductVariationResponseDTO> findAllBySimilarName(String name) {
-        return productVariationRepository.findAllBySimilarName(name).stream()
-                .map(productVariationMapper::toResponse)
-                .toList();
-
+    public Page<ProductVariationResponseDTO> findAll(Pageable pageable) {
+        return productVariationRepository.findAllVariationsWithProducts(pageable)
+                .map(productVariationMapper::toResponse);
     }
 
     @Transactional(readOnly = true)
     public ProductVariationResponseDTO findById(Long id) {
-        return productVariationMapper.toResponse(productVariationRepository.findById(id)
-                .orElseThrow(() -> new NotFoundException("Product Variation not found at id: " + id)));
+        if (cache.containsKey(id))
+            return productVariationMapper.toResponse(cache.getValueOfKey(id));
+
+        ProductVariation productVariation = productVariationRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException("Product Variation not found at id: " + id));
+
+        cache.add(id, productVariation);
+
+        return productVariationMapper.toResponse(productVariation);
     }
 
     @Transactional
@@ -62,6 +66,15 @@ public class ProductVariationService {
     @Transactional
     public ProductVariationResponseDTO delete(Long id) {
         try {
+            if (cache.containsKey(id)) {
+                ProductVariation productVariation = cache.getValueOfKey(id);
+
+                productVariationRepository.delete(productVariation);
+                cache.remove(id);
+
+                return productVariationMapper.toResponse(productVariation);
+            }
+
             ProductVariation productVariation = productVariationRepository.getReferenceById(id);
 
             productVariationRepository.delete(productVariation);
@@ -75,12 +88,15 @@ public class ProductVariationService {
     @Transactional
     public ProductVariationResponseDTO update(Long id, ProductVariationRequestDTO newProduct) {
         try {
-            ProductVariation productVariation = productVariationRepository.getReferenceById(id);
+            //isso ficou lindo, sou foda.
+            ProductVariation productVariation = cache.containsKey(id) ? cache.getValueOfKey(id) : productVariationRepository.getReferenceById(id);
 
             productVariation.setColor(newProduct.getColor());
             productVariation.setDeposit(newProduct.getDeposit());
             productVariation.setImageUrl(newProduct.getImageUrl());
-            // nao vou mudar o produto, porque nao faz sentido
+
+            if (cache.containsKey(id))
+                cache.update(id, productVariation);
 
             return productVariationMapper.toResponse(productVariationRepository.save(productVariation));
         } catch (EntityNotFoundException exception) {
@@ -92,6 +108,10 @@ public class ProductVariationService {
     @Transactional(readOnly = true)
     protected ProductVariation getEntityById( Long id) {
         try {
+
+            if (cache.containsKey(id))
+                return cache.getValueOfKey(id);
+
             return productVariationRepository.getReferenceById(id);
         } catch (EntityNotFoundException exception) {
             throw new NotFoundException("Produto Variado nao Encontrado com id: " + id);
